c098bc3c80b8042c127bd2eb79890a560577e026
Nikhilesh Prasannakumar <nikhilesh.prasannakumar@gmail.com>
2016-07-20 13:14:08 +0530

    Updated gitignore to exclude CCS metadata

--- 00-blink/Debug/subdir_rules.mk
+++ 00-blink/Debug/subdir_rules.mk
@@ -6,7 +6,7 @@
 blink.obj: ../blink.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/nitp/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/bin/cl430" -vmsp --abi=eabi --use_hw_mpy=none --include_path="C:/nitp/ti/ccsv6/ccs_base/msp430/include" --include_path="C:/nitp/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include" --advice:power=all -g --define=__MSP430G2553__ --diag_warning=225 --diag_wrap=off --display_error_number --printf_support=minimal --preproc_with_compile --preproc_dependency="blink.pp" $(GEN_OPTS__FLAG) "$<"
+	"C:/nitp/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/bin/cl430" -vmsp --abi=eabi --use_hw_mpy=none --include_path="C:/nitp/ti/ccsv6/ccs_base/msp430/include" --include_path="C:/nitp/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include" --advice:power=all -g --define=__MSP430G2553__ --diag_warning=225 --display_error_number --diag_wrap=off --printf_support=minimal --preproc_with_compile --preproc_dependency="blink.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
