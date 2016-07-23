684ca97c1ec2dbbc30cc33b4e36bfe7317819c9c
Nikhilesh Prasannakumar <nikhilesh.prasannakumar@gmail.com>
2016-07-23 13:24:48 +0530

    fixed latch issue in shift reg

--- 24-spiShift/main.c
+++ 24-spiShift/main.c
@@ -11,21 +11,22 @@ void main(void)
 	UCA0CTL1 |= UCSWRST;                     		// Hold USCI in reset state
 	UCA0CTL0 |= UCCKPL + UCMST + UCSYNC;  			// 3-pin, 8-bit, SPI Master
 	UCA0CTL1 |= UCSSEL_2;                     		// Clock -> SMCLK
-	UCA0BR0 |= 0x02;                          		// SPI CLK -> SMCLK/2
-	UCA0BR1 = 0;                              		//
-	UCA0MCTL = 0;                             		// No modulation
+	UCA0BR0 = 0x02;                          		// SPI CLK -> SMCLK/2
 	UCA0CTL1 &= ~UCSWRST;                     		// Initialise USCI module
 
 	while(1)
 	{
 		P1OUT &= ~BIT5;								// Set Latch to LOW
-		while (!(IFG2 & UCA0TXIFG));				// Check if TX in progress
+		while (!(IFG2 & UCA0TXIFG));				// Check if TX Buffer is empty
 		UCA0TXBUF = 0xAA;							// Transmit first pattern
+		while ((UCA0STAT & UCBUSY));				// Wait till TX Completes
 		P1OUT |= BIT5;								// Set Latch to HIGH
 		__delay_cycles(500000);						// Delay 500 ms
+
 		P1OUT &= ~BIT5;								// Set Latch to LOW
-		while (!(IFG2 & UCA0TXIFG));				// Check if TX in progress
+		while (!(IFG2 & UCA0TXIFG));				// Check if TX Buffer is empty
 		UCA0TXBUF = 0x55;							// Transmit second pattern
+		while ((UCA0STAT & UCBUSY));				// Wait till TX Completes
 		P1OUT |= BIT5;								// Set Latch to HIGH
 		__delay_cycles(500000);						// Delay 500 ms
 	}
